223d5cccca403d204f602d1bdbcd894bba7c9d53
Akhtyam Sakaev <ahtyam.s@livetex.ru>
2016-01-31 16:50:34 +0300

    Handlers refactoring

--- server.js
+++ server.js
@@ -1,29 +1,6 @@
 var thrift = require('thrift');
 var DummyService = require('./gen-nodejs/DummyService');
-var fs = require('fs');
+var handlers = require('./lib/handlers');
 
-
-var server = thrift.createServer(DummyService, {
-  ping: (result) => {
-    console.log('ping()');
-    result();
-  },
-
-  uploadBinaryData: (filename, data, result) => {
-    console.log('filename: ' + filename);
-    console.log('data length: ', data.length);
-
-    var path = './uploaded_' + filename;
-    fs.writeFile(path, data, (error) => {
-      console.log('The file was saved!');
-
-      if (error) {
-        result(null, false);
-      } else {
-        result(null, true);
-      }
-    });
-  }
-});
-
+var server = thrift.createServer(DummyService, handlers);
 server.listen(9090);
--- server_http.js
+++ server_http.js
@@ -1,47 +1,20 @@
 var thrift = require('thrift');
 var DummyService = require('./gen-nodejs/DummyService');
-var fs = require('fs');
-
-
-var dummyHandler = {
-  ping: (result) => {
-    console.log('ping()');
-    result();
-  },
-
-  uploadBinaryData: (filename, data, result) => {
-    console.log('filename: ' + filename);
-    console.log('data length: ', data.length);
-
-    var path = './uploaded_' + filename;
-    fs.writeFile(path, data, (error) => {
-      console.log('The file was saved!');
-
-      if (error) {
-        result(null, false);
-      } else {
-        result(null, true);
-      }
-    });
-  }
-};
-
+var handlers = require('./lib/handlers');
 
 var dummySvcOpt = {
-  handler: dummyHandler,
+  handler: handlers,
   processor: DummyService,
   protocol: thrift.TBinaryProtocol,
   transport: thrift.TBufferedTransport
 };
 
-
 var serverOpt = {
   services: {
-    "/dummy": dummySvcOpt
+    '/dummy': dummySvcOpt
   }
 };
 
-
 var port = 9090;
 thrift.createWebServer(serverOpt).listen(port);
-console.log("Http/Thrift Server running on port: " + port);
+console.log('Http/Thrift Server running on port: ' + port);
